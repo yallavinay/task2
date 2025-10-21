d615a28c1acb6e0517d8c9c2a185e0d6caef26c0
yallavinay <22a91a04p9@aec.edu.in>
2025-10-21 21:22:26 +0530

    updated code

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,6 @@ describe('GET /api/message', () => {
   it('returns the expected JSON message', async () => {
     const response = await request(app).get('/api/message');
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ message: 'Hello from Jenkins CI/CD Node.js app!' });
+    expect(response.body).toEqual({ message: 'Hello i am vinay Currently purssing b tech in ECE' });
   });
 });
